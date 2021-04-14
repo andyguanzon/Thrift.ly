41597f51f5ac617f6c98572dca80272e73879627
andyguanzon <78843306+andyguanzon@users.noreply.github.com>
2021-04-14 13:16:56 +0800

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,19 +1,25 @@
 import database as db
 
 def login(username, password):
-	is_valid_login = False
-	user=None
-	temp_user = db.get_user(username)
-	if(temp_user != None):
-		if(temp_user["password"]==password):
-			is_valid_login=True
-			user={"username":username,"password":password, "interests":interests,"following":following}
-	return is_valid_login, user
+	def login(username, password):
+    is_valid_login = False
+    user=None
+    temp_user = db.get_user(username)
+    if(temp_user != None):
+        if(temp_user["password"]==password):
+            is_valid_login=True
+            user={"username":username,
+                  "interests":temp_user["interests"],
+                  "following":temp_user["following"]}
+
+    return is_valid_login, user
 
 def signup(username, password):
 	is_valid_signup = False
 	user=None
 	temp_user = db.get_user(username)
 	if temp_user is None:
-		user={"username":username, "password":password,"interests":"","following":""}
+		user={"username":username, "password":password,
+		"interests":temp_user["interests"],
+		"following":temp_user["following"]}
 	return is_valid_signup, user
